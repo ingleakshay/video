ba192dfc0c1e116e92f0c8aee5b669a38db97302
Allan Espinosa <allan.espinosa@outlook.com>
2017-09-07 00:56:40 -0400

    Add newline to the response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const port = 3000
 
 const requestHandler = (request, response) => {
-  response.end('Hello from inside Docker!!!')
+  response.end('Hello from inside Docker!!!\n')
 }
 
 const server = http.createServer(requestHandler)
